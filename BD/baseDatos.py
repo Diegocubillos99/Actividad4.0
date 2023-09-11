a69915114cae636e658f259864396b326e5652f1
Diegocubillos99 <143672541+Diegocubillos99@users.noreply.github.com>
2023-09-10 23:47:22 -0500

    Update baseDatos.py

--- BD/baseDatos.py
+++ BD/baseDatos.py
@@ -1,9 +1,9 @@
-from pymongo.mongo_client 
-import MongoClient
+from pymongo.mongo_client #Establecer la conexión con una base de datos MongoDB
+import MongoClient #Establecer la conexión con una base de datos MongoDB
 
 #CONEXIÓN
-def conexion():
-    uri = "mongodb+srv://davidmateo0509:1234@cluster0.afpjypb.mongodb.net/?retryWrites=true&w=majority"
+def conexion(): #Establecer la conexión con una base de datos MongoDB
+    uri = "mongodb+srv://davidmateo0509:1234@cluster0.afpjypb.mongodb.net/?retryWrites=true&w=majority" #Servidor MongoDB del compañero del grupo donde se almacearon los datos de las diferentes deformaciones
     # Create a new client and connect to the server
     return MongoClient(uri)
 
@@ -15,9 +15,9 @@ def conexion():
 """ Código necesario para crear un read en MongoDB"""
 def lecturaDatos():
     client = conexion()
-    db = client.actividad4.data_real
-    data_list = []
-    for data_real_bd in db.find():
+    db = client.actividad4.data_real #Acceder a una base de datos llamada actividad4 y a la colección data_real
+    data_list = [] #Los datos se agregan a la lista 
+    for data_real_bd in db.find(): #Consulta de todos los registros 
         data_list.append(data_real_bd)
     return data_list
 
